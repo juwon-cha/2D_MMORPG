869405474d6d0530dc4667f262668f2ab6bf3005
Yechan kim <dpcks5959@naver.com>
2025-02-01 14:47:12 +0900

    [ADD] Bitconverter 유틸 클래스 추가

--- 2D_MMO_Server/ServerCore/BitConverter.cpp
+++ 2D_MMO_Server/ServerCore/BitConverter.cpp
@@ -2,13 +2,13 @@
 #include "BitConverter.h"
 
 template <typename T>
-bool WriteBytes(T value, byte* bytes, uint32 startIndex) {
+bool WriteBytes(T value, BYTE* bytes, uint32 startIndex) {
 	::memcpy(&bytes[startIndex], &value, sizeof(value));
 	return true;
 }
 
 template <typename T>
-T ToInt(byte* bytes, uint32 startIndex) {
+T ToInt(BYTE* bytes, uint32 startIndex) {
 	T value;
 	::memcpy(&value, &bytes[startIndex], sizeof(value));
 	return value;
@@ -16,82 +16,82 @@ T ToInt(byte* bytes, uint32 startIndex) {
 
 namespace Utils
 {
-	bool BitConverter::TryWriteBytes(byte* destination, uint8 value, uint32 startIndex)
+	bool BitConverter::TryWriteBytes(BYTE* destination, uint8 value, uint32 startIndex)
 	{
 		return WriteBytes(value, destination, startIndex);
 	}
 
-	bool BitConverter::TryWriteBytes(byte* destination, uint16 value, uint32 startIndex)
+	bool BitConverter::TryWriteBytes(BYTE* destination, uint16 value, uint32 startIndex)
 	{
 		return WriteBytes(value, destination, startIndex);
 	}
 
-	bool BitConverter::TryWriteBytes(byte* destination, uint32 value, uint32 startIndex)
+	bool BitConverter::TryWriteBytes(BYTE* destination, uint32 value, uint32 startIndex)
 	{
 		return WriteBytes(value, destination, startIndex);
 	}
 
-	bool BitConverter::TryWriteBytes(byte* destination, uint64 value, uint32 startIndex)
+	bool BitConverter::TryWriteBytes(BYTE* destination, uint64 value, uint32 startIndex)
 	{
 		return WriteBytes(value, destination, startIndex);
 	}
 
-	bool BitConverter::TryWriteBytes(byte* destination, int8 value, uint32 startIndex)
+	bool BitConverter::TryWriteBytes(BYTE* destination, int8 value, uint32 startIndex)
 	{
 		return WriteBytes(value, destination, startIndex);
 	}
 
-	bool BitConverter::TryWriteBytes(byte* destination, int16 value, uint32 startIndex)
+	bool BitConverter::TryWriteBytes(BYTE* destination, int16 value, uint32 startIndex)
 	{
 		return WriteBytes(value, destination, startIndex);
 	}
 
-	bool BitConverter::TryWriteBytes(byte* destination, int32 value, uint32 startIndex)
+	bool BitConverter::TryWriteBytes(BYTE* destination, int32 value, uint32 startIndex)
 	{
 		return WriteBytes(value, destination, startIndex);
 	}
 
-	bool BitConverter::TryWriteBytes(byte* destination, int64 value, uint32 startIndex)
+	bool BitConverter::TryWriteBytes(BYTE* destination, int64 value, uint32 startIndex)
 	{
 		return WriteBytes(value, destination, startIndex);
 	}
 
-	uint8 BitConverter::ToUInt8(byte* arr, int32 startIndex)
+	uint8 BitConverter::ToUInt8(BYTE* arr, int32 startIndex)
 	{
 		return ToInt<uint8>(arr, startIndex);
 	}
 
-	uint16 BitConverter::ToUInt16(byte* arr, int32 startIndex)
+	uint16 BitConverter::ToUInt16(BYTE* arr, int32 startIndex)
 	{
 		return ToInt<uint16>(arr, startIndex);
 	}
 
-	uint32 BitConverter::ToUInt32(byte* arr, int32 startIndex)
+	uint32 BitConverter::ToUInt32(BYTE* arr, int32 startIndex)
 	{
 		return ToInt<uint32>(arr, startIndex);
 	}
 
-	uint64 BitConverter::ToUInt64(byte* arr, int32 startIndex)
+	uint64 BitConverter::ToUInt64(BYTE* arr, int32 startIndex)
 	{
 		return ToInt<uint64>(arr, startIndex);
 	}
 
-	int8 BitConverter::ToInt8(byte* arr, int32 startIndex)
+	int8 BitConverter::ToInt8(BYTE* arr, int32 startIndex)
 	{
 		return ToInt<int8>(arr, startIndex);
 	}
 
-	int16 BitConverter::ToInt16(byte* arr, int32 startIndex)
+	int16 BitConverter::ToInt16(BYTE* arr, int32 startIndex)
 	{
 		return ToInt<int16>(arr, startIndex);
 	}
 
-	int32 BitConverter::ToInt32(byte* arr, int32 startIndex)
+	int32 BitConverter::ToInt32(BYTE* arr, int32 startIndex)
 	{
 		return ToInt<int32>(arr, startIndex);
 	}
 
-	int64 BitConverter::ToInt64(byte* arr, int32 startIndex)
+	int64 BitConverter::ToInt64(BYTE* arr, int32 startIndex)
 	{
 		return ToInt<int64>(arr, startIndex);
 	}
--- 2D_MMO_Server/ServerCore/BitConverter.h
+++ 2D_MMO_Server/ServerCore/BitConverter.h
@@ -2,24 +2,24 @@
 
 namespace Utils
 {
-	class BitConverter
+	class COREDLL BitConverter
 	{
 	public:
-		static bool TryWriteBytes(byte* destination, uint8 value, uint32 startIndex);
-		static bool TryWriteBytes(byte* destination, uint16 value, uint32 startIndex);
-		static bool TryWriteBytes(byte* destination, uint32 value, uint32 startIndex);
-		static bool TryWriteBytes(byte* destination, uint64 value, uint32 startIndex);
-		static bool TryWriteBytes(byte* destination, int8 value, uint32 startIndex);
-		static bool TryWriteBytes(byte* destination, int16 value, uint32 startIndex);
-		static bool TryWriteBytes(byte* destination, int32 value, uint32 startIndex);
-		static bool TryWriteBytes(byte* destination, int64 value, uint32 startIndex);
-		static uint8 ToUInt8(byte* arr, int32 startIndex = 0);
-		static uint16 ToUInt16(byte* arr, int32 startIndex = 0);
-		static uint32 ToUInt32(byte* arr, int32 startIndex = 0);
-		static uint64 ToUInt64(byte* arr, int32 startIndex = 0);
-		static int8 ToInt8(byte* arr, int32 startIndex = 0);
-		static int16 ToInt16(byte* arr, int32 startIndex = 0);
-		static int32 ToInt32(byte* arr, int32 startIndex = 0);
-		static int64 ToInt64(byte* arr, int32 startIndex = 0);
+		static bool TryWriteBytes(BYTE* destination, uint8 value, uint32 startIndex);
+		static bool TryWriteBytes(BYTE* destination, uint16 value, uint32 startIndex);
+		static bool TryWriteBytes(BYTE* destination, uint32 value, uint32 startIndex);
+		static bool TryWriteBytes(BYTE* destination, uint64 value, uint32 startIndex);
+		static bool TryWriteBytes(BYTE* destination, int8 value, uint32 startIndex);
+		static bool TryWriteBytes(BYTE* destination, int16 value, uint32 startIndex);
+		static bool TryWriteBytes(BYTE* destination, int32 value, uint32 startIndex);
+		static bool TryWriteBytes(BYTE* destination, int64 value, uint32 startIndex);
+		static uint8 ToUInt8(BYTE* arr, int32 startIndex = 0);
+		static uint16 ToUInt16(BYTE* arr, int32 startIndex = 0);
+		static uint32 ToUInt32(BYTE* arr, int32 startIndex = 0);
+		static uint64 ToUInt64(BYTE* arr, int32 startIndex = 0);
+		static int8 ToInt8(BYTE* arr, int32 startIndex = 0);
+		static int16 ToInt16(BYTE* arr, int32 startIndex = 0);
+		static int32 ToInt32(BYTE* arr, int32 startIndex = 0);
+		static int64 ToInt64(BYTE* arr, int32 startIndex = 0);
 	};
 }
