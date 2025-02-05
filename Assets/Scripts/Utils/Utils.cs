4579749d9fdc3b22bfbb80c18abf6d6efa921327
JuwonCha <littlebigcha@gmail.com>
2025-02-05 15:38:05 +0900

    [ADD] 맵 데이터 변환 툴 추가

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 
-public class Utils : MonoBehaviour
+public class Utils
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameObject FindChild(GameObject go, string name)
     {
-        
+        Transform transform = FindChild<Transform>(go, name);
+
+        if(transform == null)
+        {
+            return null;
+        }
+
+        return transform.gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static T FindChild<T>(GameObject go, string name) where T : Object
     {
-        
+        if(go == null)
+        {
+            return null;
+        }
+
+        for(int i = 0; i < go.transform.childCount; i++)
+        {
+            Transform transform = go.transform.GetChild(i);
+
+            if(string.IsNullOrEmpty(name) || transform.name == name)
+            {
+                T component = transform.GetComponent<T>();
+                if(component != null)
+                {
+                    return component;
+                }
+            }
+        }
+
+        return null;
     }
 }
