0a77eb371e22193dc6770f31f1efc6e62d26547a
Yechan kim <dpcks5959@naver.com>
2025-02-06 23:47:11 +0900

    [MODIFY] dll export 누락 처리

--- 2D_MMO_Server/ServerCore/format.h
+++ 2D_MMO_Server/ServerCore/format.h
@@ -1,10 +1,12 @@
 #ifndef FORMAT_H
 #define FORMAT_H
+
+#include "pch.h"
 #include <initializer_list>
 
 namespace Utils {
 	namespace {
-		static void ReplaceString(string& input, uint32 i, const string& val)
+		void ReplaceString(string& input, uint32 i, const string& val)
 		{
 			string target = "{" + to_string(i) + "}";
 			uint64 pos = input.find(target);
@@ -20,7 +22,7 @@ namespace Utils {
 			}
 		}
 
-		static void ReplacewString(wstring& input, uint32 i, const wstring& val)
+		void ReplacewString(wstring& input, uint32 i, const wstring& val)
 		{
 			wstring target = L"{" + to_wstring(i) + L"}";
 			uint64 pos = input.find(target);
@@ -36,7 +38,7 @@ namespace Utils {
 			}
 		}
 		template <typename T>
-		static wstring wformatHandler(const string& input, const initializer_list<T>& args) {
+		wstring wformatHandler(const string& input, const initializer_list<T>& args) {
 			wstring result;
 			uint32 i = 0;
 
@@ -50,7 +52,7 @@ namespace Utils {
 			return result;
 		}
 	}
-	static string format(const string& input, const initializer_list<string>& args) {
+	COREDLL string format(const string& input, const initializer_list<string>& args) {
 		string result = input;
 		uint32 i = 0;
 
@@ -61,7 +63,7 @@ namespace Utils {
 		}
 		return result;
 	}
-	static wstring wformat(const wstring& input, const initializer_list<wstring>& args) {
+	COREDLL wstring wformat(const wstring& input, const initializer_list<wstring>& args) {
 		wstring result = input;
 		uint32 i = 0;
 
@@ -72,16 +74,16 @@ namespace Utils {
 		}
 		return result;
 	}
-	static wstring wformat(const string& input, const initializer_list<string>& args) {
+	COREDLL wstring wformat(const string& input, const initializer_list<string>& args) {
 		wstring result;
 		string t = format(input, args);
 		result.assign(t.begin(), t.end());
 		return result;
 	}
-	static wstring wformat(const string& input, const initializer_list<uint64>& args) {
+	COREDLL wstring wformat(const string& input, const initializer_list<uint64>& args) {
 		return wformatHandler(input, args);
 	}
-	static wstring wformat(const string& input, const initializer_list<uint32>& args) {
+	COREDLL wstring wformat(const string& input, const initializer_list<uint32>& args) {
 		return wformatHandler(input, args);
 	}
 }
