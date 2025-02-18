e362765255c23f9f899605b52062eb01c4220a9d
JuwonCha <littlebigcha@gmail.com>
2025-02-19 00:18:24 +0900

    [ADD] 이동 패킷 처리

--- 2D_MMO_Server/GameServer/PacketHandler.cpp
+++ 2D_MMO_Server/GameServer/PacketHandler.cpp
@@ -9,5 +9,30 @@
 
 void PacketHandler::C_MOVEHandler(PacketSession* session, ByteRef buffer)
 {
+	ClientSession* clientSession = static_cast<ClientSession*>(session);
+	auto movePkt = GetRoot<C_MOVE>(buffer->operator BYTE * ());
+	cout << "C_MOVE (" << movePkt->posInfo()->posX() << ", " << movePkt->posInfo()->posY() << ")" << endl;
 
+	Player* player = clientSession->GetPlayer().get();
+	if (player == nullptr)
+	{
+		return;
+	}
+	if (player->GetGameRoom() == nullptr)
+	{
+		return;
+	}
+
+	// 서버에서 좌표 저장(이동)
+	{
+		flatbuffers::FlatBufferBuilder builder;
+		auto name = builder.CreateString(player->GetPlayerName());
+		auto posInfo = CreatePositionInfo(builder, movePkt->posInfo()->state(), movePkt->posInfo()->moveDir(), movePkt->posInfo()->posX(), movePkt->posInfo()->posY());
+
+		// 다른 플레이어들에게 알려줌
+		auto move = CreateSC_MOVE(builder, player->GetPlayerId(), posInfo);
+		auto respondMovePkt = PacketManager::Instance().CreatePacket(move, builder, PacketType_SC_MOVE);
+
+		player->GetGameRoom()->Broadcast(respondMovePkt);
+	}
 }
