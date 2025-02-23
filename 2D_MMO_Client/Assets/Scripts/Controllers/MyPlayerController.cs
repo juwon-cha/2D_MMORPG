a8f2bd94f6dfafd635b3fa6fabc9abe7e8045a83
JuwonCha <littlebigcha@gmail.com>
2025-02-23 11:37:37 +0900

    [FIX] UnityEditor 네임스페이스 빌드 오류

--- 2D_MMO_Client/Assets/Scripts/Controllers/MyPlayerController.cs
+++ 2D_MMO_Client/Assets/Scripts/Controllers/MyPlayerController.cs
@@ -1,6 +1,8 @@
 using Google.FlatBuffers;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MyPlayerController : PlayerController
 {
@@ -71,6 +73,7 @@ public class MyPlayerController : PlayerController
         }
     }
 
+    // This script runs only in the Unity editor
     private void ExitPlayMode()
     {
 #if UNITY_EDITOR
