d703bcd5cc71b6f39900c6c30df8336f00fbeb6f
JuwonCha <littlebigcha@gmail.com>
2025-06-07 18:50:50 +0900

    [CHORE] 파일 정리

---
+++ Common/DBServer/PacketManager.cpp
@@ -0,0 +1,51 @@
+
+#include "pch.h"
+#include "PacketManager.h"
+#include "PacketHandler.h"
+#include "CoreRef.h"
+
+PacketManager* PacketManager::_instance = nullptr;
+
+PacketManager& PacketManager::Instance()
+{
+	if (_instance == nullptr)
+		_instance = new PacketManager();
+	return *_instance;
+}
+
+PacketManager::PacketManager()
+{
+	Register();
+}
+
+PacketManager::~PacketManager()
+{
+}
+
+void PacketManager::Register()
+{
+    _handler[PacketType::PacketType_SD_SIGNUP] = PacketHandler::SD_SIGNUPHandler;
+		_handler[PacketType::PacketType_SD_SIGNIN] = PacketHandler::SD_SIGNINHandler;
+		
+}
+
+void PacketManager::OnRecvPacket(PacketSession* session, BYTE* buffer)
+{
+	uint16 count = 0;
+
+	uint16 size = Utils::BitConverter::ToUInt16(buffer, count);
+	count += sizeof(uint16);
+	uint16 id = Utils::BitConverter::ToUInt16(buffer, count);
+	count += sizeof(uint16);
+
+	auto func = _handler.find(id);
+	if (func != _handler.end())
+	{
+		size -= count;
+		ByteRef buf = make_shared<BaseRef<BYTE>>();
+
+		buf->Reserve(size);
+		buf->Copy(buffer + count, size);
+		func->second(session, buf);
+	}
+}
---
+++ Common/Server/PacketManager.cpp
@@ -0,0 +1,59 @@
+
+#include "pch.h"
+#include "PacketManager.h"
+#include "PacketHandler.h"
+#include "CoreRef.h"
+
+PacketManager* PacketManager::_instance = nullptr;
+
+PacketManager& PacketManager::Instance()
+{
+	if (_instance == nullptr)
+		_instance = new PacketManager();
+	return *_instance;
+}
+
+PacketManager::PacketManager()
+{
+	Register();
+}
+
+PacketManager::~PacketManager()
+{
+}
+
+void PacketManager::Register()
+{
+    _handler[PacketType::PacketType_C_ENTER_GAME] = PacketHandler::C_ENTER_GAMEHandler;
+		_handler[PacketType::PacketType_C_MOVE] = PacketHandler::C_MOVEHandler;
+		_handler[PacketType::PacketType_C_CHAT] = PacketHandler::C_CHATHandler;
+		_handler[PacketType::PacketType_C_SKILL] = PacketHandler::C_SKILLHandler;
+		_handler[PacketType::PacketType_C_EQUIP_ITEM] = PacketHandler::C_EQUIP_ITEMHandler;
+		_handler[PacketType::PacketType_C_CHANGE_MAP] = PacketHandler::C_CHANGE_MAPHandler;
+		_handler[PacketType::PacketType_C_SIGNUP] = PacketHandler::C_SIGNUPHandler;
+		_handler[PacketType::PacketType_D_SIGNUP] = PacketHandler::D_SIGNUPHandler;
+		_handler[PacketType::PacketType_C_SIGNIN] = PacketHandler::C_SIGNINHandler;
+		_handler[PacketType::PacketType_D_SIGNIN] = PacketHandler::D_SIGNINHandler;
+		
+}
+
+void PacketManager::OnRecvPacket(PacketSession* session, BYTE* buffer)
+{
+	uint16 count = 0;
+
+	uint16 size = Utils::BitConverter::ToUInt16(buffer, count);
+	count += sizeof(uint16);
+	uint16 id = Utils::BitConverter::ToUInt16(buffer, count);
+	count += sizeof(uint16);
+
+	auto func = _handler.find(id);
+	if (func != _handler.end())
+	{
+		size -= count;
+		ByteRef buf = make_shared<BaseRef<BYTE>>();
+
+		buf->Reserve(size);
+		buf->Copy(buffer + count, size);
+		func->second(session, buf);
+	}
+}
